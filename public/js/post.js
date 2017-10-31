a5afe7beea5b60ded09b06e28ddaf163fb815141
Jonathan Tung <jtung23@gmail.com>
2017-10-31 00:13:09 -0700

    set up post route for pairing

--- public/js/post.js
+++ public/js/post.js
@@ -1,27 +1,47 @@
 $(document).ready(function() {
+
+	// when submit button is clicked at input
 	$('#submit').on('click', function() {
 		var alch = $('#alchInput').val().trim();
 		var food = $('#foodInput').val().trim();
-		var rating = $('#rating').val().trim();
-		var review = $('#review').val().trim();
+		var newRating = $('#rating').val().trim();
+		var newReview = $('#review').val().trim();
 
-		getFoodId(food);
-		getAlchId(alch);
-
-		$.get('/api/alcohol' + alch, function(data) {
-			console.log('/ ALCOHOL DATA /', data);
-
-		});
+		getFoodId(food, alch, getAlchId);
 
 		var newPairing = {};
 	});
 });
 
-var getFoodId = function(food, callback) {
+// Queries Food table and pulls food id
+var getFoodId = function(food, alch, callback) {
 	var foodQuery = "/?food_name=" + food;
 	$.get('/api/food' + foodQuery, function(data) {
 		console.log('/ FOOD DATA /', data);
-
+		// callback goes to getalchId function with food id
+		callback(data, alch, postNewPairing);
 	});
 
 }
+
+// Queries Alcohol Table and pulls alch id
+var getAlchId = function(foodId, alch, callback) {
+	var alchQuery = "/?alc_name=" + alch;
+	$.get('/api/alcohol' + alchQuery, function(data) {
+		console.log('/ ALCH DATA /', data);
+		// callback goes to postNewPairing function with food and alch id
+		callback(food, data);
+	})
+}
+
+// posts the new pairing with the food and alch ids
+var postNewPairing = function(foodId, alchId) {
+	var newPairing = {
+		alc_id: alchId,
+		food_id: foodId,
+		rating: newRating,
+		review: newReview
+	};
+	$.post('/api/pairing', newPairing);
+
+};
--- routes/post-routes.js
+++ routes/post-routes.js
@@ -4,13 +4,18 @@ module.exports = function(app) {
 	app.get('/api/food', function(req, res) {
 		var query = {};
 		query.food_name = req.query.food_name;
-		
+		console.log('food get QUERY:', query)
 	})
 
-	app.post('/api/posts', function(req, res) {
+	app.get('/api/alcohol', function(req, res) {
+		var query = {};
+		query.alc_name = req.query.alc_name;
+		console.log('alch get QUERY:', query);
+	})
+	app.post('/api/pairing', function(req, res) {
+		console.log('POST pairing REQ: ', req);
 		db.Post.create(req.body).then(function(dbPost) {
 			res.json(dbPost);
 		})		
 	});
-
 }
