4fa7f666acd7b55438df0c897afdc2b232488cf9
Jonathan Tung <jtung23@gmail.com>
2017-11-02 13:49:30 -0700

    carousel and some more front

--- public/js/html.js
+++ public/js/html.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+// initiate slick carousel
 var prevArrow = "<button type='button' class='slick-prev pull-left'><i class='fa fa-arrow-circle-left fa-3' aria-hidden='true'></i></button>"
 var nextArrow = "<button type='button' class='slick-next pull-right'><i class='fa fa-arrow-circle-right fa-3' aria-hidden='true'></i></button>"
 $('.multiple-items').slick({
@@ -9,5 +10,8 @@ $('.multiple-items').slick({
   prevArrow: prevArrow,
   nextArrow: nextArrow
 });
+
+// post modal
+// logic: searches in food, if postfood runs then changes modal to have input which(function) sends back to post modal
 
 });
