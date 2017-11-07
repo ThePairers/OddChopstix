49189ef5582cc8c14d50121c023de0838591c3ac
marina lyulko <marinalyulko@gmail.com>
2017-11-06 18:12:55 -0800

    deleted dropzone

--- server.js
+++ server.js
@@ -51,25 +51,4 @@ cloudinary.config({
 
 
 
-// var myDropzone = new Dropzone(document.getElementById('dropzone-area'), {
-// 	uploadMultiple: false,
-// 	acceptedFiles:'.jpg,.png,.jpeg,.gif',
-// 	parallelUploads: 6,
-// 	url: 'https://api.cloudinary.com/v1_1/oddchopstix/image/upload'
-// });
 
-
-// myDropzone.on('sending', function (file, xhr, formData) {
-// 	formData.append('api_key', 637332173612438);
-// 	formData.append('timestamp', Date.now() / 1000 | 0);
-// 	formData.append('upload_preset', 'preset1');
-// });
-// myDropzone.on('success', function (file, response) {
-// 	console.log('Success! Cloudinary public ID is', response.public_id);
-// });
-
-cloudinary.uploader.upload("./public/images/jameson.jpg", {tags: "example"})
-    .then(function(error, result) {
-    	console.log(result);
-    	console.log(error);
-    });
